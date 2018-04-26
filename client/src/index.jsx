829b7896618e44fe0a8921575b0cce28f816df8f
Mark Khuzam <khuzammark@gmail.com>
2018-04-25 19:38:54 -0700

    fixed rebase issues

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,12 +6,6 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Sling from "./components/Sling/index.jsx";
 import Auth from "./components/Auth/Signup.jsx";
 import App from "./App.jsx";
-import thunk from "redux-thunk";
-import { createStore, applyMiddleware, compose } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import storage from "redux-persist/lib/storage";
-import allReducers from "./store/index.js";
 import "./index.css";
 
 ReactDOM.render(
